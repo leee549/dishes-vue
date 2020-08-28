212690b75c8f90b6419bafd475cdcfa9e227a8ae
leee549 <1193106371@qq.com>
2020-08-28 22:20:07 +0800

    all

---
+++ src/i18n/langs/en.js
@@ -0,0 +1,6 @@
+const en={
+  username:'Please enter username',
+    password:'Please enter password',
+    captcha:'Please enter captcha',
+}
+export default en
---
+++ src/i18n/langs/zh.js
@@ -0,0 +1,6 @@
+const zh={
+  username:'请输入用户名',
+    password:'请输入密码',
+    captcha:'请输入验证码',
+}
+export default zh
